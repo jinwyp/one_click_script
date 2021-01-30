e6b6216f6eb63ac717af42a0776ade3feb32d70b
JinWYP <jinwyp@gmail.com>
2021-01-30 22:39:25 +0800

    U-update

--- trojan_v2ray_install.sh
+++ trojan_v2ray_install.sh
@@ -3424,7 +3424,7 @@ function start_menu(){
     fi
 
     green " =================================================="
-    green " Trojan Trojan-go V2ray 一键安装脚本 2020-12-6 更新.  系统支持：centos7+ / debian9+ / ubuntu16.04+"
+    green " Trojan Trojan-go V2ray 一键安装脚本 2021-01-30 更新.  系统支持：centos7+ / debian9+ / ubuntu16.04+"
     red " *请不要在任何生产环境使用此脚本 请不要有其他程序占用80和443端口"
     red " *若是已安装trojan 或第二次使用脚本，请先执行卸载trojan"
     green " =================================================="
