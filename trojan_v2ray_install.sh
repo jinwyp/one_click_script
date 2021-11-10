f0b670e5cbc9d7227a665a528481bfb2cac3be7e
JinWYP <jinwyp@gmail.com>
2021-11-10 18:57:06 +0800

    U-update

--- trojan_v2ray_install.sh
+++ trojan_v2ray_install.sh
@@ -454,6 +454,7 @@ name=nginx repo
 baseurl=https://nginx.org/packages/centos/$osReleaseVersionNo/\$basearch/
 gpgcheck=0
 enabled=1
+sslverify=0
 
 EOF
         if ! rpm -qa | grep -qw iperf3; then
